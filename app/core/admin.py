68f9af0299caf315ac281eeebbdd8b995813eb7a
Shalini Roy <shaliniroy012@gmail.com>
2015-01-14 16:36:39 +0530

    Added models

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from core.models import Main, Size, Colour, Dresses_type, Item
 
 # Register your models here.
+admin.site.register(Main)
+admin.site.register(Size)
+admin.site.register(Colour)
+admin.site.register(Dresses_type)
+admin.site.register(Item)
--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from core.utils import get_upload_file_path
+from django.utils.translation import ugettext_lazy as _
 
 # Create your models here.
+
+class Main(models.Model):
+    category = models.CharField(max_length=50)
+
+class Dresses_type(models.Model):
+    subcategory = models.CharField(max_length=50)
+    main = models.ForeignKey(Main)
+
+class Colour(models.Model):
+    colour = models.CharField(max_length=50)
+
+class Size(models.Model):
+    size = models.CharField(max_length=50)
+
+class Item(models.Model):
+    item_name = models.CharField(max_length=50)
+    color = models.ForeignKey(Colour)
+    size = models.ForeignKey(Size)
+    price = models.CharField(max_length=50)
+    picture = models.ImageField(_('Image'), upload_to=get_upload_file_path)
+    type = models.ForeignKey(Dresses_type)
