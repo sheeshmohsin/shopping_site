d015b2684c87be5ebef0c5756b59200af58503d3
Shalini Roy <shaliniroy012@gmail.com>
2015-01-16 12:42:06 +0530

    Added model for seller

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from core.models import Main, Size, Colour, Dresses_type, Item
+from core.models import Main, Size, Colour, Dresses_type, Seller, Item
 
 # Register your models here.
 admin.site.register(Main)
 admin.site.register(Size)
 admin.site.register(Colour)
 admin.site.register(Dresses_type)
+admin.site.register(Seller)
 admin.site.register(Item)
--- app/core/models.py
+++ app/core/models.py
@@ -12,21 +12,38 @@ class Main(models.Model):
 
 class Dresses_type(models.Model):
     subcategory = models.CharField(_('Subcategory'), max_length=50)
-    main = models.ForeignKey(Main)
+    fk_main = models.ForeignKey(Main)
 
     def __unicode__(self):
         return self.subcategory
 
 class Colour(models.Model):
-    colour = models.CharField(_(Colour), max_length=50)
+    colour = models.CharField(_('Colour'), max_length=50)
 
 class Size(models.Model):
-    size = models.CharField(_(Size), max_length=50)
+    size = models.CharField(_('Size'), max_length=50)
 
-class Item(models.Model):
-    item_name = models.CharField(_(Item), max_length=50)
-    color = models.ForeignKey(Colour)
-    size = models.ForeignKey(Size)
-    price = models.CharField(_(Price), max_length=50)
-    picture = models.ImageField(_('Image'), upload_to=get_upload_file_path)
-    type = models.ForeignKey(Dresses_type)
+class Seller(models.Model):
+    name = models.CharField(_('Name'), max_length=50)
+    address = models.CharField(_('Address'), max_length=50)
+    email = models.EmailField(_('Email Address'), max_length=50)
+    contactno = models.IntegerField(_('Contact No.'), max_length=10)
+
+class Item(models.Model):  
+    created = models.CharField(_('Created'), max_length=50)
+    item_name = models.CharField(_('Item'), max_length=50)
+    description = models.CharField(_('Description'), max_length=50)
+    fk_color = models.ForeignKey(Colour)
+    fk_size = models.ForeignKey(Size)
+    price = models.CharField(_('Price'), max_length=50)
+    discount = models.CharField(_('Discount'), max_length=50)
+    picture1 = models.ImageField(_('Image1'), upload_to=get_upload_file_path)
+    picture2 = models.ImageField(_('Image2'), upload_to=get_upload_file_path)
+    picture3 = models.ImageField(_('Image3'), upload_to=get_upload_file_path)
+    picture4 = models.ImageField(_('Image3'), upload_to=get_upload_file_path)
+    instock = models.CharField(_('Instock'), max_length=50)
+    cod = models.CharField(_('COD'), max_length=50)
+    fk_seller = models.ForeignKey(Seller)
+    fk_type = models.ForeignKey(Dresses_type)
+
+
