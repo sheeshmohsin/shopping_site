fb931845721b460eb0f405ed80bdea603b85fd9f
Sheesh Mohsin <sheeshmohsin@gmail.com>
2015-01-29 22:40:37 +0530

    Pushed static files

--- app/global/views.py
+++ app/global/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render, render_to_response
 from core.models import *
+
 # Create your views here.
+
 def home(request):
 	categories = Main.objects.all()
 	return render_to_response('home.html', {'categories':categories})
 
 def item(request):
 	return render_to_response('item.html')
+
+def catalog(request, pk):
+	category = Main.objects.get(id=pk)
+	subcategories = category.dresses_type_set.all()
+	return render_to_response('catalog.html', {'subcategories':subcategories})
+
+def feedback(request):
+	return render_to_response('feedback.html')
