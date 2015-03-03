439eb2e2979d69dbe6ca9b4a8e0cbb9a7b039f42
Sheesh Mohsin <sheeshmohsin@gmail.com>
2015-03-04 00:32:30 +0530

    Added related_items row in item page

--- app/static/js/item.js
+++ app/static/js/item.js
@@ -6,3 +6,25 @@ $(document).ready(function() {
 		auto:true     
 	});  
 });
+$(document).ready(function(){
+  $('#myCarousel').carousel();
+});
+$('#myCarousel').carousel({
+  interval: 4000
+});
+$('.carousel .aitem').each(function(){
+  var next = $(this).next();
+  if (!next.length) {
+    next = $(this).siblings(':first');
+  }
+  next.children(':first-child').clone().appendTo($(this));
+  
+  for (var i=0;i<2;i++) {
+    next=next.next();
+    if (!next.length) {
+      next = $(this).siblings(':first');
+    }
+    
+    next.children(':first-child').clone().appendTo($(this));
+  }
+});
